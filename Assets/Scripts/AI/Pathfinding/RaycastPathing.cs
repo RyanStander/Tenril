c7aa5c724e38c80102a8dbaa85a73a2d49f70466
Ryan <stander.ryan@gmail.com>
2021-08-31 11:39:36 +0200

    TestingScriptMergeRyan

--- Assets/Scripts/AI/Pathfinding/RaycastPathing.cs
+++ Assets/Scripts/AI/Pathfinding/RaycastPathing.cs
@@ -7,12 +7,17 @@ public class RaycastPathing : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hello World");
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void TestHelloWorld()
+    {
+        Debug.Log("Hello world says hello world");
     }
 }
