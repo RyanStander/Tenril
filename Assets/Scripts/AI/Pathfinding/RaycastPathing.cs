d422c753d7da0eeace9740b29e7298684571beae
Jacques <jacques.adriaan.venter@gmail.com>
2021-08-31 11:40:29 +0200

    TestingScriptMergeJacques

--- Assets/Scripts/AI/Pathfinding/RaycastPathing.cs
+++ Assets/Scripts/AI/Pathfinding/RaycastPathing.cs
@@ -7,12 +7,19 @@ public class RaycastPathing : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hello World!");
+        TestRandomNumberPrint();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    //Test function for GitKraken script merging
+    private void TestRandomNumberPrint()
+    {
+        Debug.Log("My random number is " + Random.Range(1,11) + "!");
     }
 }
