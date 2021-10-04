c5d4e97864a4600b6737ed19ce89498d99a1a8f4
Ryan <stander.ryan@gmail.com>
2021-10-04 19:15:39 +0200

    Pickup script improvement
    Made it so that object pickups functionality uses less code.

--- Assets/Scripts/Interactables/WeaponPickup.cs
+++ Assets/Scripts/Interactables/WeaponPickup.cs
@@ -1,12 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
-using UnityEngine.UI;
 
-public class WeaponPickup : Interactable
+public class WeaponPickup : ItemPickup
 {
-    public WeaponItem weapon;
 
     public override void Interact(PlayerManager playerManager)
     {
@@ -15,27 +12,18 @@ public class WeaponPickup : Interactable
         PickUpItem(playerManager);
     }
 
-    private void PickUpItem(PlayerManager playerManager)
+    protected override void PickUpItem(PlayerManager playerManager)
     {
-        PlayerInventory playerInventory;
-        PlayerAnimatorManager playerAnimatorManager;
+        base.PickUpItem(playerManager);
 
-        playerInventory = playerManager.GetComponent<PlayerInventory>();
-        playerAnimatorManager = playerManager.GetComponent<PlayerAnimatorManager>();
-
-        //Plays the animation of picking up item
-        playerAnimatorManager.PlayTargetAnimation("PickUp", true);
-        //Add weapon to inventory
-        playerInventory.weaponsInventory.Add(weapon);
-
-        //Notify player of obtaining item
-        playerManager.itemInteractableGameObject.GetComponentInChildren<TextMeshProUGUI>().text = "You obtained " + weapon.itemName + "!";
-        //Set the weapon icon to display item
-        playerManager.itemInteractableGameObject.GetComponentInChildren<RawImage>().texture = weapon.itemIcon.texture;
-        //Enable the game object
-        playerManager.itemInteractableGameObject.SetActive(true);
-
-        //Destroy the interactable after pick up
-        Destroy(gameObject);
+        if (item is WeaponItem weapon)
+        {
+            //Add weapon to inventory
+            playerInventory.weaponsInventory.Add(weapon);
+        }
+        else
+        {
+            Debug.LogWarning("The item given for WeaponPickup is not a weapon, please fix, kthanks");
+        }
     }
 }
