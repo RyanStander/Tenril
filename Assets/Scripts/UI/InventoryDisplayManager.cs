0e131080f4cbd70f9ea550fe73cefe761392f3db
Ryan <stander.ryan@gmail.com>
2021-10-13 16:18:30 +0200

    Inventory Sorting

--- Assets/Scripts/UI/InventoryDisplayManager.cs
+++ Assets/Scripts/UI/InventoryDisplayManager.cs
@@ -12,13 +12,18 @@ public class InventoryDisplayManager : MonoBehaviour
 
     private void OnEnable()
     {
+        UpdateGoldDisplay();
+
         LoadAllInventoryToDisplay();
     }
 
-    private void LoadAllInventoryToDisplay()
+    private void UpdateGoldDisplay()
     {
         goldDisplay.text = playerInventory.Gold.ToString();
+    }
 
+    public void LoadAllInventoryToDisplay()
+    {
         //removes all previous items
         foreach (Transform child in inventoryContentObject.transform)
         {
@@ -40,4 +45,61 @@ public class InventoryDisplayManager : MonoBehaviour
             }
         }
     }
+
+    public void LoadWeaponInventoryToDisplay()
+    {
+        //removes all previous items
+        foreach (Transform child in inventoryContentObject.transform)
+        {
+            Destroy(child.gameObject);
+        }
+
+        foreach (Item item in playerInventory.Inventory)
+        {
+            //check if the item is a weapon item
+            if (item is WeaponItem weapon)
+            {
+                //create inventory item
+                GameObject createdInventoryItemPrefab = Instantiate(inventoryItemDisplayPrefab, inventoryContentObject.transform);
+
+                //Get the inventory display script
+                InventoryItemDisplay inventoryItemDisplay = createdInventoryItemPrefab.GetComponent<InventoryItemDisplay>();
+
+                //check if an inventoryitemdisplay script is present
+                if (inventoryItemDisplay != null)
+                {
+                    inventoryItemDisplay.LoadValues(item.itemName, item.itemIcon);
+                }
+            }
+        }
+    }
+
+    public void LoadConsumableInventoryToDisplay()
+    {
+        //removes all previous items
+        foreach (Transform child in inventoryContentObject.transform)
+        {
+            Destroy(child.gameObject);
+        }
+
+        foreach (Item item in playerInventory.Inventory)
+        {
+            //check if the item is a weapon item
+            if (item is QuickslotItem consumable)
+            {
+                //create inventory item
+                GameObject createdInventoryItemPrefab = Instantiate(inventoryItemDisplayPrefab, inventoryContentObject.transform);
+
+                //Get the inventory display script
+                InventoryItemDisplay inventoryItemDisplay = createdInventoryItemPrefab.GetComponent<InventoryItemDisplay>();
+
+                //check if an inventoryitemdisplay script is present
+                if (inventoryItemDisplay != null)
+                {
+                    inventoryItemDisplay.LoadValues(item.itemName, item.itemIcon);
+                }
+            }
+        }
+    }
+
 }
