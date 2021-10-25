0f91b39a07656793c94636d4a4321f28b06b764e
Jacques <jacques.adriaan.venter@gmail.com>
2021-10-25 21:01:13 +0200

    UtilityRaycast Partially Implemented
    Raycast capable of detecting nearby objects and rating their desirability. No movement or desire for target object is currently implemented.

---
+++ Assets/Scripts/AI/UtilityRay.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UtilityRay
+{
+    //Constructor
+    public UtilityRay(Ray givenRay, float givenUtility, Color givenColor)
+    {
+        baseRay = givenRay;
+        rayUtility = givenUtility;
+        rayColor = givenColor;
+    }
+
+    //Ray that contains ray utilityRay information
+    public Ray baseRay;
+
+    //The utility value (favorability) of the ray
+    public float rayUtility;
+
+    //The color that the ray (if debbuging) should have
+    public Color rayColor;
+}
