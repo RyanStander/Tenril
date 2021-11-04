525825165bda5280d5fa312ef60de5200125ffff
Jacques <jacques.adriaan.venter@gmail.com>
2021-11-04 16:01:48 +0100

    Created generalized CharacterManager

---
+++ Assets/Scripts/CharacterManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterManager : MonoBehaviour
+{
+    public Transform lockOnTransform;
+
+    //The inventory of the enemy
+    protected WeaponSlotManager weaponSlotManager;
+}
