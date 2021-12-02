b6a49f93d471bc455529669b06acb24ed3e4e471
Jacques <jacques.adriaan.venter@gmail.com>
2021-12-02 15:52:38 +0100

    Cleaned up Script "Using"
    Removed using statements that were not being used.

--- Assets/Scripts/AI/FSM/EnemyFSM.cs
+++ Assets/Scripts/AI/FSM/EnemyFSM.cs
@@ -1,6 +1,4 @@
-using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Localization.Plugins.XLIFF.V20;
 using UnityEngine;
 using UnityEngine.AI;
 public class EnemyFSM : MonoBehaviour
